aefe4536647838b5201f231704267418cf6457d5
Torah <95339155+Termng@users.noreply.github.com>
2024-01-17 10:51:52 +0100

    refactored path operations

--- app/main.py
+++ app/main.py
@@ -14,19 +14,16 @@ models.Base.metadata.create_all(bind=engine)
 
 
 
-
-
-
 app = FastAPI()
 
 class Posts(BaseModel):
-    title:str
+    post:str
     content: str
-    published: bool = True
+    is_published: bool = True
 
 while True:   
     try:
-        conn = psycopg2.connect(host='localhost', database = 'my fastAPI' , user = 'postgres', password = 'Merciful', cursor_factory=RealDictCursor)
+        conn = psycopg2.connect(host='localhost', database = 'Torah' , user = 'postgres', password = 'Merciful', cursor_factory=RealDictCursor)
         cursor = conn.cursor()
         print('Connection to db was successful')
         break
@@ -52,36 +49,39 @@ def get_all_posts(db: Session = Depends(get_db)):
 # PATH OPERATION FOR CREATING POSTS
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_post(postSchema: Posts, db: Session = Depends(get_db)):
-    
-    uploaded_post = models.Post(post = postSchema.title, content = postSchema.content, is_published = postSchema.published)
-    
-    db.add(uploaded_post)
+def create_posts(pyPosts: Posts, db: Session = Depends(get_db)):
+    create_posts = models.Post(**pyPosts.dict())
+    db.add(create_posts)
     db.commit()
-    db.refresh(uploaded_post)
-    return {"uploaded": uploaded_post}
-
+    db.refresh(create_posts)
+    return{"my_output": create_posts}
     # cursor.execute(""" INSERT INTO posts (title, content, is_published) VALUES (%s, %s, %s) RETURNING *  """, (postSchema.title, postSchema.content, postSchema.published))
     # uploaded_post = cursor.fetchone()
     # conn.commit()
+    
+    
+    
+    
     
   
 # PATH OPERATION FOR GETTING POSTS BY ID
 
 @app.get("/posts/{id}")
-def get_single_post(id: int):
-    cursor.execute(""" SELECT * FROM posts WHERE id = %s  """, (str(id), ))
-    single_post = cursor.fetchone()
+def get_single_post(id: int, db: Session = Depends(get_db)):
+    get_one_post = db.query(models.Post).filter(models.Post.id == id).first()
     
-    if not single_post:
+    # cursor.execute(""" SELECT * FROM posts WHERE id = %s  """, (str(id), ))
+    # single_post = cursor.fetchone()
+    
+    if not get_one_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f'post with the id: {id} was not found')
-    return{"single": single_post}
+    return{"single": get_one_post}
 
 
 # PATH OPERATION FOR DELETING POSTS BY ID
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
+def delete_post(id: int, db: Session = Depends(get_db)):
     cursor.execute(""" DELETE FROM posts WHERE id = %s RETURNING * """, (str(id), ))
     deleted = cursor.fetchone()
     conn.commit()
